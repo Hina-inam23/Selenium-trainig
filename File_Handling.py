e50d09650df70a6e2d9d40954165a0c89ea3f9ef
Hina-inam23 <43783883+Hina-inam23@users.noreply.github.com>
2018-11-30 19:17:14 +0500

    First seleniunm commit

---
+++ File_Handling.py
@@ -0,0 +1,8 @@
+
+def main():
+    f = open("demofile.txt", "r")
+    print(f.read())
+
+
+if __name__== "__main__":
+    main()
---
+++ read_file.py
@@ -0,0 +1,11 @@
+import sys
+def main():
+    f=open("demofile.txt","r")
+    print(f.read())
+    #fl = f.readlines()
+    #for x in fl:
+     #    print(x)
+
+if __name__ == '__main__':
+    main()
+
