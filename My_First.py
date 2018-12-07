e38018c05c8fd60c4f23f24ced191a2f408f59a1
ABDUL-MAROOF-LT\AbdulMaroof <abdul.maroof@cloudplex.io>
2018-12-08 00:03:20 +0500

    Testing branch

--- My_First.py
+++ My_First.py
@@ -1,8 +1,9 @@
-from selenium import webdriver
-import HtmlTestRunner
-import unittest
-from selenium.webdriver.common.keys import Keys
 import time
+import unittest
+import HtmlTestRunner
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 class Login(unittest.TestCase):
 
@@ -10,21 +11,19 @@ class Login(unittest.TestCase):
         self.browser = webdriver.Firefox()
         self.browser.get("https://www.gmail.com/")
 
-
     def test_Login(self):
         username = self.browser.find_element_by_id("identifierId")
         username.send_keys("hina.inam@tkxel.com")
         login_attempt = self.browser.find_element_by_class_name('CwaK9').click()
         time.sleep(5)
         password = self.browser.find_element_by_class_name("whsOnd.zHQkBf")
-        password.send_keys("!tkxel23!")
+        password.send_keys("!!")
         login_attempt =self.browser.find_element_by_class_name("CwaK9").click()
-
-
+		
     def tearDown(self):
         self.browser.quit()
-
 
 if __name__ == '__main__':
     testRunner=HtmlTestRunner.HTMLTestRunner(output=html_report_dir))
     HtmlTestRunner.main()
+	
